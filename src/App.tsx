e2a83ba84a3f4954e938b9acc9bfa717c59c2e28
jake <guswns1853@gmail.com>
2021-03-16 01:04:04 +0900

    feat: add blueprint.js and fast-refresh option(for development env)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App">Start!</div>;
-}
+import "normalize.css/normalize.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+
+import { Button } from "@blueprintjs/core";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button type="button" intent="primary">
+        Start!
+      </Button>
+    </div>
+  );
+};
 
 export default App;
