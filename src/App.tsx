6504b11b1dc9d938cee1eab24fc1525c15a3476e
jake <guswns1853@gmail.com>
2021-03-18 00:15:34 +0900

    test: add styled-components library, styled-components and antd test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import "antd/dist/antd.css";
-import { Button } from "antd";
+import styled from "styled-components";
+import { Card, Layout, Menu } from "antd";
+const { Header, Sider, Content } = Layout;
+
+const StyledSider = styled(Sider)`
+  overflow: auto;
+  height: 90vh;
+  left: 0;
+`;
+
+const StyledCard = styled(Card)`
+  padding: 0;
+`;
 
 const App = () => {
   return (
     <div className="App">
-      <Button type="primary">test</Button>
+      <StyledCard>
+        <Layout>
+          <StyledSider>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </StyledSider>
+          <Layout>
+            <Header></Header>
+            <Content></Content>
+          </Layout>
+        </Layout>
+      </StyledCard>
     </div>
   );
 };
