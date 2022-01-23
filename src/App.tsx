cb1c503e1993824f073a1fc53e87e85fd60b6317
jake <guswns1853@gmail.com>
2022-01-23 10:01:46 +0900

    fix: layout 변경, 관리 방향성 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,52 @@
 import "antd/dist/antd.css";
-import { Layout, Menu, Card, Avatar } from "antd";
+import { Layout, Menu, Avatar } from "antd";
 import Icon from "./components/Icon";
-import {
-  StyledLayout,
-  StyledHeader,
-  StyledSider,
-  StyledContent,
-} from "./components/Layout";
+import { StyledLayout, StyledSider, StyledContent } from "./components/Layout";
 import { StyledItem } from "./components/Menu";
 import { mapAvatarColor, mapMenus } from "./constants";
+const { Header } = Layout;
 
 const App = () => {
   return (
     <div className="App">
-      <Card style={{ border: 0 }}>
-        <StyledLayout>
-          <StyledSider>
-            {mapAvatarColor.map((avatarColor) => {
-              return (
-                <Avatar
-                  key={avatarColor.key}
-                  size={12}
-                  style={{
-                    backgroundColor: avatarColor.color,
-                    marginLeft: "8px",
-                  }}
-                />
-              );
-            })}
-            <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub"]}
-            >
-              <div style={{ height: "30px" }}></div>
-              <Menu.SubMenu key="sub" title="Jake's Portfolio">
-                {mapMenus.map((menu) => {
-                  return (
-                    <StyledItem key={menu.key}>
-                      <Icon type={menu.icon} style={{ marginRight: "8px" }} />
-                      {menu.text}
-                    </StyledItem>
-                  );
-                })}
-              </Menu.SubMenu>
-            </Menu>
-          </StyledSider>
-          <Layout>
-            <StyledHeader></StyledHeader>
-            <StyledContent></StyledContent>
-          </Layout>
-        </StyledLayout>
-      </Card>
+      <StyledLayout>
+        <StyledSider>
+          {mapAvatarColor.map((avatarColor) => {
+            return (
+              <Avatar
+                key={avatarColor.key}
+                size={12}
+                style={{
+                  backgroundColor: avatarColor.color,
+                  marginLeft: "8px",
+                }}
+              />
+            );
+          })}
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub"]}
+          >
+            <div style={{ height: "30px" }}></div>
+            <Menu.SubMenu key="sub" title="Jake's Note">
+              {mapMenus.map((menu) => {
+                return (
+                  <StyledItem key={menu.key}>
+                    <Icon type={menu.icon} style={{ marginRight: "8px" }} />
+                    {menu.text}
+                  </StyledItem>
+                );
+              })}
+            </Menu.SubMenu>
+          </Menu>
+        </StyledSider>
+        <Layout>
+          <Header></Header>
+          <StyledContent></StyledContent>
+        </Layout>
+      </StyledLayout>
     </div>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,18 @@
 import styled from "styled-components";
 import { Layout } from "antd";
-const { Header, Sider, Content } = Layout;
+const { Sider, Content } = Layout;
 
 export const StyledLayout = styled(Layout)`
   background-color: #fff;
 `;
 
-export const StyledHeader = styled(Header)`
-  border-radius: 0 10px 0 0;
-`;
-
 export const StyledSider = styled(Sider)`
   overflow: auto;
-  height: 90vh;
+  height: 100vh;
   left: 0;
-  border-radius: 10px 0 0 10px;
   padding: 18px 12px;
 `;
 
 export const StyledContent = styled(Content)`
-  border-radius: 0 0 10px 0;
   background-color: #f0f2f5;
 `;
